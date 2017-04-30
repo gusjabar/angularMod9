fafe4e1066616c889db7fa4c7a4407532cbe045c
Gustavo <jg_barreto@hotmail.com>
2017-04-30 19:40:15 -0400

    added spin icon

--- src/app/post.component.ts
+++ src/app/post.component.ts
@@ -5,20 +5,29 @@ import 'rxjs/add/operator/map';
 
 @Component({
     selector: 'post',
-    template: '',
+    template: `<div [hidden]='isLoading'>
+                    Getting post..
+                    <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
+                    <span class="sr-only">Loading...</span>
+               </div>`,
     providers: [PostService]
 })
 export class PostComponent implements OnInit {
+    isLoading = false;
     constructor(private _postService: PostService) {
-        this._postService.createPost({
-            userId: 1,
-            title: 'T2',
-            body: 'b2'
-        })
+        // this._postService.createPost({
+        //     userId: 1,
+        //     title: 'T2',
+        //     body: 'b2'
+        // })
     }
     ngOnInit() {
         this._postService
             .getPost()
-            .subscribe(x => console.log(x[0].body));
+            .delay(3000)
+            .subscribe(post => {
+                this.isLoading = true;
+                console.log(post)
+            });
     }
 }
